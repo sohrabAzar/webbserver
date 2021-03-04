364438903f51ad53b117f9831d3e5e62f0abfdbe
Sohrab Azar <sohrabsa@gmail.com>
2021-03-04 14:29:58 +0100

    instead of making check que in server a task make the action in client a task

--- Uppgift 8 (webbserver)/Program.cs
+++ Uppgift 8 (webbserver)/Program.cs
@@ -11,16 +11,9 @@ namespace Uppgift_8__webbserver_
         /// Checks Console to see if a keyboard key has been pressed, if so returns it as uppercase, otherwise returns '\0'.
         /// </summary>  
         static char ReadKeyIfExists() => Console.KeyAvailable ? Console.ReadKey(intercept: true).Key.ToString().ToUpper()[0] : '\0';
-
-        private static Webserver myWebServer = new Webserver();
-
-        // field used to contol the amount of time messages send to server
-        internal static int sendMessageTimeCounter = 0;
-
-        // dummy variable to increase the work load of the message send to server 
-        private static int dummy = 0;
         static void Main(string[] args)
         {
+            myWebServer.QueueCheck += QueueCheckEventHandler;
             // Set to control the interval at which a message is send to server
             const int sendMessageToServerTimeInterval = 100;
 
@@ -32,7 +25,6 @@ namespace Uppgift_8__webbserver_
 
             while (!quit)
             {
-
                 // quit program on Q press
                 sendMessageTimeCounter += 1;
                 pressedKey = ReadKeyIfExists();
@@ -44,10 +36,41 @@ namespace Uppgift_8__webbserver_
 
                 // Simulate Client sending a message into the queue
                 SendNewMessageToServer(sendMessageToServerTimeInterval);
-
+               
                 Thread.Sleep(10);
             }
         }
+
+        #region SERVER
+        private static Webserver myWebServer = new Webserver();
+
+        /// <summary>
+        /// Show message on UI when server checks queue
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="messageFound"></param>
+        private static void QueueCheckEventHandler(object sender, bool messageFound)
+        {
+            if (messageFound)
+            {
+                Console.WriteLine("message handeled");
+                Console.WriteLine($"{myWebServer.MyQueue.Count} messages are in queue\n");
+
+            }
+            else
+            {
+                Console.WriteLine("queue is empty");
+            }
+            // Console.WriteLine(messageFound? "message handeled": "queue is empty");
+        }
+        #endregion  
+
+        #region CLIENT
+        // field used to contol the amount of time messages send to server
+        internal static int sendMessageTimeCounter = 0;
+
+        // dummy variable to increase the work load of the message send to server 
+        private static int dummy = 0;
 
         /// <summary>
         /// Send message to server in specified time intervals
@@ -57,27 +80,41 @@ namespace Uppgift_8__webbserver_
         {
             if (sendMessageTimeCounter > timeInterval)
             {
-                Action x =  new Action(ActionHandler_Light);                
+                Action x = new Action(ActionHandler_HeavyAsync);
                 myWebServer.MyQueue.Enqueue(x);
                 Console.WriteLine("\nClient sent a message\n");
                 sendMessageTimeCounter = 0;
             }
         }
 
-        // Two types of actions when send message to server, one heavy and one light
-        private static void ActionHandler_Heavy()
+        // Three types of actions when send message to server, one heavy and one light
+        private static void ActionHandler_HeavyAsync()
         {
+            Task.Run(() =>
+            {
                 for (int i = 0; i < 1000000000; i++)
                 {
                     dummy += 1;
                 }
                 Console.WriteLine("\nClient sent a message\n");
                 dummy = 0;
+            });
+
+        }
+        private static void ActionHandler_Heavy()
+        {
+            for (int i = 0; i < 1000000000; i++)
+            {
+                dummy += 1;
+            }
+            Console.WriteLine("\nClient sent a message\n");
+            dummy = 0;
 
         }
         private static void ActionHandler_Light()
         {
             dummy += 1;
         }
+        #endregion
     }
 }
--- Uppgift 8 (webbserver)/Webserver.cs
+++ Uppgift 8 (webbserver)/Webserver.cs
@@ -9,9 +9,25 @@ namespace Uppgift_8__webbserver_
     
     public class Webserver
     {
+        #region MEMBERS
+        /// <summary>
+        /// Time between each attemp at reading message queue by serve in seconds
+        /// </summary>
+        /// 
+        private const int serverUpdateTimeInterval = 1;
 
-        private Timer aTimer = new System.Timers.Timer(2000);
+        /// <summary>
+        /// Timer used to run the servers tick method
+        /// </summary>
+        private Timer aTimer = new System.Timers.Timer(serverUpdateTimeInterval * 1000);
 
+        /// <summary>
+        /// Server message queue
+        /// </summary>
+        public Queue<Action> MyQueue { get; set; } = new Queue<Action>();
+        #endregion
+
+        #region METHODS
         public void StartServer()
         {
             aTimer.Elapsed += Tick;
@@ -20,27 +36,36 @@ namespace Uppgift_8__webbserver_
 
         private void Tick(object sender, ElapsedEventArgs e)
         {
-            Task task = Task.Run(CheckQueue);
-            // CheckQueue();
+            CheckQueue();
         }
-
-        public Queue<Action> MyQueue { get; set; } = new Queue<Action>();
-
+       
         public void CheckQueue()
         {
-            
+            bool messageFound = false;
+
             if (MyQueue.Count > 0)
-            {                
-                Console.WriteLine("message handeled");
+            {
+                //Console.WriteLine("message handeled");
                 MyQueue.Dequeue().Invoke();
-                Console.WriteLine($"{MyQueue.Count} messages are in queue\n");              
+                //Console.WriteLine($"{MyQueue.Count} messages are in queue\n");
+                messageFound = true;
             }
             else
             {
-                Console.WriteLine("queue is empty\n");
+                // Console.WriteLine("queue is empty\n");
             }
 
+            OnQueueChecked(messageFound);
         }
 
+        /// <summary>
+        /// Event used to talk to UI and show message when queue is checked
+        /// </summary>
+        public event EventHandler<bool> QueueCheck;
+        protected void OnQueueChecked(bool messageFound)
+        {
+            QueueCheck?.Invoke(this, messageFound);
+        }
+        #endregion
     }
 }
